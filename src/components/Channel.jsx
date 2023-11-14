28a382c25c72f8ee7d5ddd93baa91cc602acb65c
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2023-11-14 13:08:23 -0300

    dev of channel page and channel card

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import ChannelCard from './ChannelCard'
+import VideoList from './VideoList'
+import { fetchFromAPI } from '../utils/fetchData'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+const customStyle = {
+  scale: "1.2",
+   marginTop: '-50px', 
+   backgroundColor: "transparent", 
+   pointerEvents: "none" 
+}
+
 
 function Channel() {
+  const [channelInfo, setChannelInfo] = useState(null)
+  const [videos, setVideos] = useState([])
+  const {id} = useParams()
+
+  useEffect(()=> {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then( data => setChannelInfo(data.items[0]))
+      .catch( err => console.log(err))
+
+      fetchFromAPI(`search?part=snippet&channelId=${id}&order=date`)
+      .then( data => setVideos(data.items))
+      .catch( err => console.log(err))
+
+  },[id])
+
+  
   return (
-    <div>Channel</div>
+    <div className='channelPage'>
+        <div  className='channelBanner'>
+          <img src={channelInfo?.brandingSettings?.image?.bannerExternalUrl}></img>
+        </div>
+       <ChannelCard customStyle={customStyle} channel={channelInfo} ></ChannelCard>
+       { VideoList && <VideoList videoList={videos}></VideoList>}
+    </div>
   )
 }
 
--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,7 +1,16 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom"
 
-function ChannelCard() {
+function ChannelCard({channel, customStyle}) {
   return (
-    <div>ChannelCard</div>
+    <div className="channelCard videoCard " style={customStyle}>
+      <Link to={ `/Chutub/channel/${channel?.id?.channelId}`}>
+          <img src={channel?.snippet?.thumbnails?.high?.url }></img>
+          <h3 title={channel?.snippet?.title}>{channel?.snippet?.title}</h3>
+          {channel?.statistics?.subscriberCount && <p title={channel?.statistics?.subscriberCount}>{ parseInt(channel?.statistics?.subscriberCount).toLocaleString() + " Subscribers"}</p> }
+        {/* esta ultima linea es para reusar el componente y datos de suscriptores en el componente Channel */}
+      </Link>
+    </div>
   )
 }
 
