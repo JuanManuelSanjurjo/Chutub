4e1e33b76e6460823bae14fb629d2f27b8f088a0
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2023-11-14 17:31:49 -0300

    react-player added to packagge json

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ReactPlayer from 'react-player/youtube'
+
 
 function VideoDetails() {
   return (
-    <div>VideoDetails</div>
+    <div>
+
+
+
+    </div>
   )
 }
 
