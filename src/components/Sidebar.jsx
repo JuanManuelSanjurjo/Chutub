c39491406d06d0451d28b817b21c6b3bc93c062a
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2024-03-16 22:10:33 -0300

    fix overflow-x in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ function Sidebar({selectedCategory, setSelectedCategory}) {
       {categories.map(cat => (
         <button className={cat.name === selectedCategory ? "selected btnCategory": "btnCategory"}  key={cat.name}
             onClick={()=> setSelectedCategory(cat.name)}>
-            <i className={`catIcon  ${cat.class}`}></i>
+            <i className={`catIcon  ${cat.class}` }></i>
             <span>{cat.name}</span>
         </button>
       ))}
