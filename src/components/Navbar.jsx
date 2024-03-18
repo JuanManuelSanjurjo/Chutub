6e05b64e5adecf4c89ad6079f30bd1fc7ec6ecac
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2024-03-18 19:40:45 -0300

    changes in constants 8

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className='navBar'>
       <Link to={"/Chutub"}>
-        <img src=".src/assets/player-skip-forward.png" className='favIcon' ></img>
+        <img src="player-skip-forward.png" className='favIcon' ></img>
         <h2 className="logo">CHUTUB</h2>
       </Link>
       <SearchBar />
