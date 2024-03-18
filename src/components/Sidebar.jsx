4100e6f020e66c7c189408e2efadbe19f4039b1f
JuanManuelSanjurjo <juanmanuelsanjurjo@hotmail.com>
2024-03-18 19:32:37 -0300

    changes in constants 6

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,17 +7,11 @@ function Sidebar({selectedCategory, setSelectedCategory}) {
       {categories.map((cat, i) => (
         <button key={i} className={cat.name === selectedCategory ? "selected btnCategory": "btnCategory"}  key={cat.name}
             onClick={()=> setSelectedCategory(cat.name)}>
-            <img src={cat.src} alt={cat.name} className={`catIcon`}></img>
+            <img src={cat.src} alt={cat.name} className={`catIcon`} />
             <span>{cat.name}</span>
         </button>
 
       ))}
-      <button  className={"Anime" === selectedCategory ? "selected btnCategory": "btnCategory"} 
-            onClick={()=> setSelectedCategory("Anime")}>
-            <img src={"src/assets/anime.png"} alt={"Anime"} className={`catIcon`}></img>
-            <span>{"Anime"}</span>
-      </button>
-
       
       <p>Copyright JMS {new Date().getFullYear().toString()} </p>
     </div>
